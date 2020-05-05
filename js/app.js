2b67335db98e4f69504f111357c0f7fe4a84d07d
Joewalk <joenart2@gmail.com>
2020-05-05 04:29:26 -0700

    Adjasting scroll when nav  menu is on

--- js/app.js
+++ js/app.js
@@ -4,9 +4,16 @@ menu.addEventListener('click', () => {
     if(menu.classList.contains('rotate')) {
         menu.classList.remove('rotate')
         mobileNave.style.display = 'none';
+
+        // When the modal is hidden, we want to remain at the top of the scroll position
+        document.body.style.position = '';
+        document.body.style.top = '';
     }else {
         mobileNave.style.display = 'block';
         menu.classList.add('rotate');
+        // When the modal is shown, we want a fixed body
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${window.scrollY}px`;
     }
     console.log(menu);
 });
