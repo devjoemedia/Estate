32be75f074bf2360a95a72c9708656e56c905c95
Joewalk <joenart2@gmail.com>
2020-05-05 07:26:10 -0700

    Added auto play slide

--- js/app.js
+++ js/app.js
@@ -26,7 +26,10 @@ var mySwiper = new Swiper ('.swiper-container', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 'auto',
-    
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
