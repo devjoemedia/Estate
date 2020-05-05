a366daf2e74295e3cbc84495ab2f32b7f66856fb
Joewalk <joenart2@gmail.com>
2020-05-05 07:12:19 -0700

    Adding slides to the review section

--- js/app.js
+++ js/app.js
@@ -18,3 +18,25 @@ menu.addEventListener('click', () => {
     console.log(menu);
 });
 
+
+
+// SWIPPER
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 'auto',
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    
+  })
